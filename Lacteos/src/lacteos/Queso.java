31ff27c6cf428fa4d2f6f1783c5a7205437e2c5e
Alejandro Lledo <a.lledo@fp.safavalladolid.com>
2024-06-05 12:57:35 +0200

    Version 2, creo los constructores y los getters y setters con refactorizacion

--- Lacteos/src/lacteos/Queso.java
+++ Lacteos/src/lacteos/Queso.java
@@ -5,5 +5,61 @@ public class Queso {
 	private int cantidad_leche;
 	private double peso;
 	
+	/**
+	 * @param tipo_leche
+	 * @param cantidad_leche
+	 * @param peso
+	 */
+	public Queso(String tipo_leche, int cantidad_leche, double peso) {
+		setTipo_leche(tipo_leche);
+		setCantidad_leche(cantidad_leche);
+		setPeso(peso);
+	}
+	public Queso() {
+		
+	}
+
+	/**
+	 * @return the tipo_leche
+	 */
+	public String getTipo_leche() {
+		return tipo_leche;
+	}
+
+	/**
+	 * @param tipo_leche the tipo_leche to set
+	 */
+	public void setTipo_leche(String tipo_leche) {
+		this.tipo_leche = tipo_leche;
+	}
+
+	/**
+	 * @return the cantidad_leche
+	 */
+	public int getCantidad_leche() {
+		return cantidad_leche;
+	}
+
+	/**
+	 * @param cantidad_leche the cantidad_leche to set
+	 */
+	public void setCantidad_leche(int cantidad_leche) {
+		this.cantidad_leche = cantidad_leche;
+	}
+
+	/**
+	 * @return the peso
+	 */
+	public double getPeso() {
+		return peso;
+	}
+
+	/**
+	 * @param peso the peso to set
+	 */
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
 	
 }
